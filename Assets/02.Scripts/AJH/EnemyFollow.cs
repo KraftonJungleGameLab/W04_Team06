1b7ba1512ee166c18c12288c78ac920416c36fde
acessiane <acessiane@gmail.com>
2023-09-08 12:22:19 +0900

    Feat: 잡

---
+++ Assets/02.Scripts/AJH/EnemyFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+#region PublicVariables
+    
+#endregion
+
+#region PrivateVariables
+
+#endregion
+
+#region PublicMethods
+
+#endregion
+
+#region PrivateMethods
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+#endregion
+}
